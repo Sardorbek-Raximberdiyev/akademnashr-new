a70e87b3628666f44cb38a863ad6c348802d1971
Sardorbek-Raximberdiyev <sardorraximberdiyev9@gmail.com>
2022-11-15 23:56:22 +0500

    Hamburger link fixed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 const elSiteHeader = document.querySelector('.site-header');
 const elOpenButton = elSiteHeader.querySelector('.open-button');
+const elsSitenavItem = document.querySelectorAll('li');
 
 if (elOpenButton) {
   elOpenButton.addEventListener('click', function () {
     elSiteHeader.classList.toggle('site-header-open');
   })
 }
+
+elsSitenavItem.forEach((item)  => {
+  item.addEventListener('click', function () {
+    elSiteHeader.classList.remove('site-header-open');
+  })
+})
